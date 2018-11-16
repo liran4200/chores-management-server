58131f0e6fe4da308faca5789c8e5dcd3c916522
liran4200 <liran4200@gmail.com>
2018-11-16 15:50:49 +0200

    Add RoommateEntity class

---
+++ src/main/java/playground/logic/RoommateEntity.java
@@ -0,0 +1,85 @@
+package playground.logic;
+
+public class RoommateEntity {
+	
+	private	String	email;
+	private String	playground;
+	private String  roommateName;
+	private String	avatar;
+	private String	role;
+	private long	points;
+	
+	public RoommateEntity() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public RoommateEntity(String email, String playground, String roommateName, String avatar, String role,
+			long points) {
+		super();
+		this.email = email;
+		this.playground = playground;
+		this.roommateName = roommateName;
+		this.avatar = avatar;
+		this.role = role;
+		this.points = points;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPlayground() {
+		return playground;
+	}
+
+	public void setPlayground(String playground) {
+		this.playground = playground;
+	}
+
+	public String getRoommateName() {
+		return roommateName;
+	}
+
+	public void setRoommateName(String roommateName) {
+		this.roommateName = roommateName;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public long getPoints() {
+		return points;
+	}
+
+	public void setPoints(long points) {
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "RoommateEntity [email=" + email + ", playground=" + playground + ", roommateName=" + roommateName
+				+ ", avatar=" + avatar + ", role=" + role + ", points=" + points + "]";
+	}
+	
+	
+	
+	
+	
+}
