74a916bd669108477afadfa3f4e2bf2c0f15b565
YURI VAINSTAIN <yuri.vainstain@ibm.com>
2018-11-24 16:44:13 +0200

    added choreAlreadyExistsException class

--- src/main/java/playground/logic/ChoreAlreadyExistsException.java
+++ src/main/java/playground/logic/ChoreAlreadyExistsException.java
@@ -2,9 +2,11 @@ package playground.logic;
 
 public class ChoreAlreadyExistsException extends RuntimeException {
 
-	private static final long serialVersionUID = -6089648253391619542L;
+	private static final long serialVersionUID = 8568225773065076428L;
+	
 	
 	public ChoreAlreadyExistsException(String message) {
 		super(message);
 	}
+
 }
